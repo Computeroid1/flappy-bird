2ead1afb0e8399c422069750616b18c5583819b5
Ugochukwu <chibuzorugochukwu24@gmail.com>
2026-01-28 14:41:06 +0200

    Implemented pipe code in Main script

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Main : Node2D
 {
+	[Export]
+	public PackedScene PipeScene;
 	bool GameRunning;
 	bool GameOver;
 	int Scroll;
@@ -10,17 +12,20 @@ public partial class Main : Node2D
 	const int ScrollSpeed = 4;
 	Vector2I ScreenSize;
 	int GroundHeight;
-	Array Pipes;
+	private Godot.Collections.Array<Pipe> Pipes = new();
 	const int PipeDelay = 100;
 	const int PipeRange = 200;
 	private Bird bird;
 	private Ground ground;
+	private Random r = new Random();
 
 	public override void _Ready()
 	{
 		ScreenSize = GetWindow().Size;
 		bird = GetNode<Bird>("Bird");
 		ground = GetNode<Ground>("Ground");
+		GroundHeight = ground.GetNode<Sprite2D>("Sprite2D").Texture.GetHeight();
+		GetNode<Timer>("PipeTimer").Timeout += _OnPipeTimerTimeout;
 		NewGame();
 	}
 
@@ -35,6 +40,13 @@ public partial class Main : Node2D
 				Scroll = 0;
 			}
 			ground.Position = new Vector2(-Scroll, ground.Position.Y);
+			
+			foreach(Pipe pipe in Pipes)
+			{
+				Vector2 Pos = pipe.Position;
+				Pos.X -= ScrollSpeed;
+				pipe.Position = Pos;
+			}
 		}
 	}
 
@@ -44,6 +56,9 @@ public partial class Main : Node2D
 		GameOver = false;
 		Score = 0;
 		Scroll = 0;
+		Pipes.Clear();
+		GeneratePipes();
+		//GetNode<Timer>("PipeTimer").Start();
 		bird.Reset();
 	}
 
@@ -76,5 +91,28 @@ public partial class Main : Node2D
 		GameRunning = true;
 		bird.flying = true;
 		bird.Flap();
+		GetNode<Timer>("PipeTimer").Start();
+	}
+
+	public void _OnPipeTimerTimeout()
+	{
+		GeneratePipes();
+	}
+
+	public void GeneratePipes()
+	{
+		Pipe pipe = PipeScene.Instantiate<Pipe>();
+		Vector2 Pos = pipe.Position;
+		Pos.X = ScreenSize.X + PipeDelay;
+		Pos.Y = (ScreenSize.Y - GroundHeight)/2 + r.Next(-PipeRange, PipeRange);
+		pipe.Position = Pos;
+		pipe.Hit += BirdHit;
+		AddChild(pipe);
+		Pipes.Add(pipe);
+	}
+
+	public void BirdHit()
+	{
+		
 	}
 }
